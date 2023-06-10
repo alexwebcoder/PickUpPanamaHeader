2f052cd5a92b6754bba67dc1dc26918193a3fcc2
alexwebcoder <alexdeleon@stayup.com>
2023-06-10 15:31:21 -0700

    added progress bar

--- sandbox.js
+++ sandbox.js
@@ -45,3 +45,31 @@ for (let item of items) {
     item.addEventListener('keypress', toggleItem, false);
   }
 }
+
+//when user scrolls page, this function executes
+
+window.onscroll = function() {
+  myFunction();
+}
+
+const myFunction = () => {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById('myBar').style.width = scrolled + '%';
+}
+
+// for the sticky header animation
+
+const header = document.querySelector('.header');
+const toggleClass = 'is-sticky';
+
+window.addEventListener('scroll', ()=> {
+   const currentScroll = window.pageYOffset;
+     if (currentScroll > 150) {
+      header.classList.add(toggleClass);
+     } else {
+      header.classList.remove(toggleClass);
+     }
+
+});
