eda204894abb6e174e454d7dc12e571c6a99ddaa
alexwebcoder <alexdeleon@stayup.com>
2023-06-22 11:56:37 -0700

    worked on link hover effects

--- sandbox.js
+++ sandbox.js
@@ -23,28 +23,28 @@ function toggleMenu() {
 toggle.addEventListener('click', toggleMenu, false);
 
 
-const items = document.querySelectorAll('.item');
+// const items = document.querySelectorAll('.item');
 
-//Activate Submenu
+// //Activate Submenu
 
-function toggleItem() {
-  if (this.classList.contains('submenu-active')) {
-    this.classList.remove('submenu-active');
-  } else if (menu.querySelector('.submenu-active')){
-    menu.querySelector('submenu-active').classList.remove('submenu-active');
-  } else {
-    this.classList.add('submenu-active');
-  } 
-}
+// function toggleItem() {
+//   if (this.classList.contains('submenu-active')) {
+//     this.classList.remove('submenu-active');
+//   } else if (menu.querySelector('.submenu-active')){
+//     menu.querySelector('submenu-active').classList.remove('submenu-active');
+//   } else {
+//     this.classList.add('submenu-active');
+//   } 
+// }
 
-//Event Listeners
+// //Event Listeners
 
-for (let item of items) {
-  if (item.querySelector('.submenu')) {
-    item.addEventListener('click', toggleItem, false);
-    item.addEventListener('keypress', toggleItem, false);
-  }
-}
+// for (let item of items) {
+//   if (item.querySelector('.submenu')) {
+//     item.addEventListener('click', toggleItem, false);
+//     item.addEventListener('keypress', toggleItem, false);
+//   }
+// }
 
 //when user scrolls page, this function executes
 
