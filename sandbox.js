b085c6a0697fdd61aee76b218ec4484f05fed722
alexwebcoder <alexdeleon@stayup.com>
2023-06-22 19:54:00 -0700

    fixed the toggle issue

--- sandbox.js
+++ sandbox.js
@@ -1,45 +1,52 @@
 const toggle = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-const openMenu = document.querySelector('.hamburger-icon');
-const closeMenu = document.querySelector('.close-btn');
+const sidenav = document.querySelector('.sidenav');
+// const openMenu = document.querySelector('.hamburger-icon');
 
 //Toggle mobile menu
 
-const openNav = () => {
-  document.querySelector('#mobileMenu').style.width = '250px';
-}
-
-const closeNav = () => {
-  document.querySelector('#mobileMenu').style.width = '0';
-}
-
-
-openMenu.addEventListener('click', () => {
-  openNav();
-});
-
-closeMenu.addEventListener('click', () => {
-  closeNav();
-})
-
-// function toggleMenu() {
-//   if (menu.classList.contains('active')) {
-//       menu.classList.remove('active');
-    
-//     //adds the menu (hamburger) icon
-//     toggle.querySelector('a').innerHTML = `<i class='fas fa-bars'></i>`;
-//   } else {
-//     menu.classList.add(`active`);
-//   //adds the close (x) icon
-//   toggle.querySelector('a').innerHTML = `<i class='fas fa-times'></i>`;
-  
-//   }
-  
+// const openNav = () => {
+//   document.querySelector('#mobileMenu').style.width = '250px';
+//   openMenu.innerHTML = `<i class='fas fa-bars'>&#9776;</i>`;
 // }
+
+// const closeNav = () => {
+//   document.querySelector('#mobileMenu').style.width = '0';
+//   openMenu.innerHTML = `<i class='fas fa-times'>&times;</i>`;
+
+// }
+
+
+// openMenu.addEventListener('click', () => {
+//   openNav();
+// });
+
+// closeMenu.addEventListener('click', () => {
+//   closeNav();
+// })
+
+function toggleMenu() {
+
+  
+  if (sidenav.classList.contains('active')) {
+    sidenav.classList.remove('active');
+    
+    //adds the menu (hamburger) icon
+    toggle.querySelector('a').innerHTML = `<i class='fas fa-bars'></i>`;
+    sidenav.style.width = '0';
+    
+  } else {
+    sidenav.classList.add(`active`);
+  //adds the close (x) icon
+  toggle.querySelector('a').innerHTML = `<i class='fas fa-times'></i>`;
+  sidenav.style.width = '250px';
+  
+  }
+  
+}
 
 //Event Listener
 
-// toggle.addEventListener('click', toggleMenu, false);
+toggle.addEventListener('click', toggleMenu, false);
 
 
 
@@ -47,24 +54,24 @@ const items = document.querySelectorAll('.item');
 
 //Activate Submenu
 
-function toggleItem() {
-  if (this.classList.contains('submenu-active')) {
-    this.classList.remove('submenu-active');
-  } else if (menu.querySelector('.submenu-active')){
-    menu.querySelector('submenu-active').classList.remove('submenu-active');
-  } else {
-    this.classList.add('submenu-active');
-  } 
-}
+// function toggleItem() {
+//   if (this.classList.contains('submenu-active')) {
+//     this.classList.remove('submenu-active');
+//   } else if (menu.querySelector('.submenu-active')){
+//     menu.querySelector('submenu-active').classList.remove('submenu-active');
+//   } else {
+//     this.classList.add('submenu-active');
+//   } 
+// }
 
-//Event Listeners
+// //Event Listeners
 
-for (let item of items) {
-  if (item.querySelector('.submenu')) {
-    item.addEventListener('click', toggleItem, false);
-    item.addEventListener('keypress', toggleItem, false);
-  }
-}
+// for (let item of items) {
+//   if (item.querySelector('.submenu')) {
+//     item.addEventListener('click', toggleItem, false);
+//     item.addEventListener('keypress', toggleItem, false);
+//   }
+// }
 
 //when user scrolls page, this function executes
 
